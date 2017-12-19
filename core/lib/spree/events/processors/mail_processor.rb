981954f5ba56d8d44c542e1cbd162c11b202151c
swcraig <swcraig.code@gmail.com>
2017-12-19 09:59:38 -0800

    Register `Spree::MailProcessor`
    This subscribes the processors to the appropriate events.

--- core/lib/spree/events/processors/mail_processor.rb
+++ core/lib/spree/events/processors/mail_processor.rb
@@ -3,6 +3,16 @@ module Spree
     module Processors
       class MailProcessor
         class << self
+          # This subscribes the MailProcessor to the relevant events in Solidus.
+          # It is called in an initializer BEFORE events start happening.
+          def register!
+            Spree.event_bus.subscribe(:order_confirmed,           ->(event) { send_confirm_email(event) })
+            Spree.event_bus.subscribe(:order_cancelled,           ->(event) { send_cancel_email(event) })
+            Spree.event_bus.subscribe(:carton_shipped,            ->(event) { send_carton_shipped_emails(event) })
+            Spree.event_bus.subscribe(:reimbursement_processed,   ->(event) { send_reimbursement_email(event) })
+            Spree.event_bus.subscribe(:order_inventory_cancelled, ->(event) { send_inventory_cancellation_email(event) })
+          end
+
           def send_confirm_email(event)
             Spree::OrderMailer.confirm_email(event.order).deliver_later unless event.order.confirmation_delivered?
             event.order.update_column(:confirmation_delivered, true)
